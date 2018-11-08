b6c6f4b13db954f514d590efa22cf5dd126b3d6b
Shiv Prakash <30807615+shivp1606@users.noreply.github.com>
2018-11-08 22:45:16 +0530

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,12 @@
+jQuery(document).ready(function(){
+
+	"use strict";
+
+	$(".testimonials-carousel").owlCarousel({
+		    autoplay: true,
+		    dots: true,
+		    loop: true,
+		    items: 1
+		  });
+
+});
